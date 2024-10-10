36a108a319b84c634c95607798a6665cd4867687
pank-su <frostkslo1@yandex.ru>
2024-10-10 12:08:21 +0300

    i am a ball

--- definition_anfun.m
+++ definition_anfun.m
@@ -1,1 +1,3 @@
+# create anonymous function from lab 2 
+
 f2 = @(x) (x < 0) .* 0 + (0 <= x & x < (pi / 4)) .* tan(x) + (x >= pi / 4) .* 1
--- f1.m
+++ f1.m
@@ -1,10 +1,33 @@
+##
+# 'y' - это нахождение кусочно заданной функции вида:
+# ```latex
+# \begin{equation}
+# y(x) = \begin{cases}
+#    0, -2 \le x < 0; \\
+#   \int\limits^{x}_0 e^{(-x + 3)^2}dx  , 0 \le x < 1; \\
+#   -2 + \sum\limits_{n=0}^{\infty} \frac{(-1)^n (x-2)^{n + 1}}{
+# (n + 1)!} , 1 \le x \le 3 
+# \end{cases}
+# \end{equation}
+# ```
+#
+# -- x - значение аргумента x
+#
+# Ответом является значение данной функции, если x - входит
+# в область определения, иначе будет ошибка
+#
+# Author: Pankov Vasya <pank@pank.su>
+##
 function y = f1(x)
+  # x - скаляр?
+  validateattributes(x, {"numeric"}, {"scalar"})
+
   if (x >= 0 && x < 1)
     y = rectInt(0, x, 0.0005, @(x) exp((-x + 3) ^ 2))
   elseif (x >= 1 && x <= 3)
     func = @(n) (x - 2.0) / n
     y = -2 + getSumInfWithE(0.000001, func, x >= 2)
-  elseif (x >= -2)
+  elseif (x >= -2 && x < 0)
     y = 0
   else
     error("Аргумент функции не принадлежит области определения функции")
